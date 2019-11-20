bef86850bb90e705b3460c535e8eac29511a0616
lim-lim-lim <limlimlim.dev@gmail.com>
2019-11-20 21:57:47 +0900

    Feat #7 implement update base

--- src/display/stage.ts
+++ src/display/stage.ts
@@ -3,7 +3,7 @@ import Container from "./container"
 export default class Stage extends Container {
 
   public get width(): number {
-    return this._width;
+    return this.width;
   }
 
   public set width(value: number) {
@@ -18,7 +18,16 @@ export default class Stage extends Container {
     this._height = value;
   }
 
+  public get backgroundColor(): string {
+    return this._backgroundColor;
+  }
+
+  public set backgroundColor(value: string) {
+    this._backgroundColor = value;
+  }
+
   private _context: CanvasRenderingContext2D;
+  private _backgroundColor: string = '#000000';
 
   constructor(
     private _canvas: HTMLCanvasElement,
@@ -26,12 +35,18 @@ export default class Stage extends Container {
     private _height?: number) {
     super();
     this._context = this._canvas.getContext('2d');
-    if (_width !== undefined) {
-      _canvas.width = _width;
+    if (_width === undefined) {
+      this._width = _canvas.width;
     }
 
-    if (_height !== undefined) {
-      _canvas.height = _height;
+    if (_height == undefined) {
+      this._height = _canvas.height;
     }
+  }
+
+  public update(): void {
+    this._context.clearRect(0, 0, this._width, this._height);
+    this._context.fillStyle = this._backgroundColor;
+    this._context.fillRect(0, 0, this._width, this._height);
   }
 }
