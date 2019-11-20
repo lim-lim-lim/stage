cc4763317aeaef3dd6ef923ea0aa047d98e509e4
lim-lim-lim <limlimlim.dev@gmail.com>
2019-11-20 21:22:47 +0900

    Feat #7 enhancement ts build env

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello Stage...!!!');
+import Display from "./display/display";
+import Container from "./display/container";
+import Bounds from "./geom/bounds";
+import Point from "./geom/point";
+
+export {
+  Stage,
+  Display,
+  Container,
+  Bounds,
+  Point
+}
--- webpack.config.js
+++ webpack.config.js
@@ -18,5 +18,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'var',
+    library: 'stage'
   },
 };
